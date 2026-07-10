fa132ff9f7d51673b97000a07d02bd65a59fd2ac
Neha <mausuminanda977@gmail.com>
2026-07-10 19:49:51 +0530

    Add TripletClosestSum and TripletSmallestSum algorithms for finding sums in arrays

---
+++ two_pointer/TripletClosestSum.java
@@ -0,0 +1,80 @@
+/*
+Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ */
+
+import java.util.*;
+public class TripletClosestSum {
+
+    public static int closetSum(int[] arr , int target){
+
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        int minDiff = Integer.MAX_VALUE;
+        int sum =0;
+
+
+        for(int i =0 ; i<n-2 ; i++){
+
+            int left = i+1;
+            int right = n-1;
+
+            while(left<right){
+            int currentSum = arr[i] + arr[left] + arr[right];
+
+            if(currentSum == target){
+                return currentSum;
+            }
+
+            int currentDiff = Math.abs(currentSum - target);
+
+            if(currentDiff < minDiff){
+                minDiff = currentDiff;
+                sum = currentSum;
+            }
+
+            if(currentSum < target){
+                left++;
+            }else
+                right--;
+
+        }
+
+        }
+
+        return sum;
+    }
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        int target = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+         
+        System.out.println(closetSum(arr, target));
+        sc.close();
+
+    }
+    
+}
---
+++ two_pointer/TripletSmallestSum.java
@@ -0,0 +1,64 @@
+/*Given an array arr[] of distinct integers and a value sum, find the count of triplets (i, j, k), having (i<j<k) with the sum of (arr[i] + arr[j] + arr[k]) smaller than the given value sum.
+
+Examples :
+
+Input: sum = 2, arr[] = [-2, 0, 1, 3]
+Output:  2
+Explanation: Triplets with sum less than 2 are (-2, 0, 1) and (-2, 0, 3). 
+Input: sum = 12, arr[] = [5, 1, 3, 4, 7]
+Output: 4
+Explanation: Triplets with sum less than 12 are (1, 3, 4), (5, 1, 3), (1, 3, 7) and (5, 1, 4).*/
+
+import java.util.*;
+
+
+public class TripletSmallestSum {
+
+    public static int smallestSum(int[] arr , int sum){
+
+        Arrays.sort(arr);
+        int n = arr.length;
+        int count = 0;
+
+        for(int i = 0; i< n-2; i++){
+
+           int left = i+1;
+           int right = n-1;
+
+           while(left < right){
+            int current_sum = arr[i] + arr[left] + arr[right];
+
+            if(current_sum < sum){
+
+                count+=right - left;
+                left++;
+            }else
+                right--;
+           }
+        }
+
+        return count;
+
+
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i =0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = sc.nextInt();
+
+        System.out.println(smallestSum(arr, sum));
+
+
+        sc.close();
+
+    }
+    
+}
