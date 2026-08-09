f3661eeaeb8958cdeb9c13fe20c8763091e5990b
Neha <mausuminanda977@gmail.com>
2026-08-09 21:35:10 +0530

    Add ReverseString algorithm to reverse a given string using a stack

---
+++ stack/ReverseString.java
@@ -0,0 +1,31 @@
+package stack;
+
+import java.util.Stack;
+
+public class ReverseString {
+
+    public static String reverseString(String str){
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i< str.length(); i++){
+            stack.push(str.charAt(i));
+        }
+
+        StringBuilder reversed = new StringBuilder();
+
+        while(!stack.isEmpty()){
+            reversed.append(stack.pop());
+        }
+
+        return reversed.toString();
+    }
+    
+    
+    public static void main(String[] args) {
+
+        String str = "hello";
+
+        System.out.println(reverseString(str));
+    }
+}
